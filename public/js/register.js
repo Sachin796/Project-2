1fb0e509be3010267268976199c951c2a12c99dc
AFWebDev <andrewfariaj@gmail.com>
2019-09-27 16:52:14 -0400

    REGISTER PAGE

--- public/js/register.js
+++ public/js/register.js
@@ -2,24 +2,4 @@ $(document).ready(function() {
   //INIT THE NAVBAR
   $(".sidenav").sidenav();
   $(".sidenav").css({ zIndex: 9999 });
-
-  $("#submit").on("click", e => {
-    let username = $("#username").val();
-    let password = $("#password").val();
-    console.log(username, password);
-    let login = {
-      username: username,
-      password: password
-    };
-
-    fetch("/register/submit", {
-      method: "POST",
-      body: JSON.stringify(login),
-      headers: {
-        "content-type": "application/json"
-      }
-    })
-      .then(res => {})
-      .catch(err => {});
-  });
 });
