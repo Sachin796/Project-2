717a1700ac17884871175682a02179c6eb0e2367
AFWebDev <andrewfariaj@gmail.com>
2019-09-27 11:36:30 -0400

    endpoints

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,8 @@ var db = require("../models");
 
 module.exports = function(app) {
   //Get all expense data where id = user id
-  //NEED AN EXPENSES TABLE.
   app.get("/api/expense/:id", (req, resp) => {
-    db.expenses
+    db.expense
       .findAll({
         where: {
           id: req.params.id
@@ -19,16 +18,50 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, resp) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/api/expense", function(req, resp) {
+    // db.Example.create(req.body).then(function(dbExample) {
+    //   resp.json(dbExample);
+    // });
+    let { id } = req.body;
+    db.expsense
+      .create(req.body)
+      .then(res => {
+        resp.json(res);
+      })
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  app.post("/register/submit", (req, resp) => {
+    let { username, password } = req.body;
+    let error = [];
+    if (!username || !password) {
+      error.push({ msg: "Please enter all fields" });
+    }
+    if (password.length < 6) {
+      error.push({ msg: "Password should be greater than 6 characters " });
+    }
+    if (error.length > 0) {
+      resp.render("registerPage", {
+        error,
+        usermame,
+        password
+      });
+    }
+    db.User.create(usermame, password)
+      .then(res => {
+        resp.json(res);
+      })
+      .catch(err => {
+        throw err;
+      });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, resp) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, resp) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, resp) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      resp.render("index", {});
-    });
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   resp.render("index", {});
+    //render our home page
+    resp.render("index");
   });
 
   //LOCATIONS ENDPOINT
   app.get("/locations", (req, resp) => {
+    db.Location.findAll({
+      where: {
+        UserId: req.body.id
+      }
+    })
+      .then(res => {})
+      .catch(err => {});
     resp.render("locationPage", { layout: "location" });
   });
 
