5e11405713db8cf98156e9b6d1b01db02b7d149c
AFWebDev <andrewfariaj@gmail.com>
2019-09-29 16:13:04 -0400

    remove garbage

--- models/user.js
+++ models/user.js
@@ -1,33 +1,24 @@
 let bcrypt = require("bcrypt");
 
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define(
-    "User",
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-
-      password: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
+  var User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
     },
-    {
-      classMethods: {
-        associate: function(models) {
-          Todo.belongsTo(models.User);
-        }
+
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
     }
-  );
+  });
+
   User.prototype.hashPassword = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
   };
