a7508ecbfa5fdafad9cff89a2176022f3bec5341
Sachin796 <sachinjaveri.2010@gmail.com>
2019-10-01 02:34:10 -0400

    Added 2 more charts styling left

--- public/js/profile.js
+++ public/js/profile.js
@@ -7,54 +7,184 @@ fetch("api/expense")
   .then(res => res.json())
   .then(result => {
     console.log(result);
-    let varchart = document.getElementById("mychart").getContext("2d");
+    createPieChart(result);
+    createLineChart(result);
+    createdoghnutChart(result);
+  });
 
-    Chart.defaults.global.defaultFontFamily = "Lato";
-    Chart.defaults.global.defaultFontFamily = 18;
-    Chart.defaults.global.defaultFontFamily = "#777";
+function createPieChart(result) {
+  let varchart = document.getElementById("mychart").getContext("2d");
 
-    let popchart = new Chart(varchart, {
-      type: "pie", //bar horizontalBar pie line doughnut radar polarArea
-      data: {
-        labels: [
-          result.categoryArr[0],
-          result.categoryArr[1],
-          result.categoryArr[2],
-          result.categoryArr[3]
-        ],
-        datasets: [
-          {
-            label: "Population",
-            data: [
-              result.expenseArr[0],
-              result.expenseArr[1],
-              result.expenseArr[2],
-              result.expenseArr[3]
-            ],
-            backgroundColor: ["green", "red", "yellow", "purple", "brown"],
-            borderWidth: 1,
-            borderColor: "darkblue",
-            hoverBorderWidth: 3,
-            hoverBorderColor: "black"
-          }
-        ]
+  Chart.defaults.global.defaultFontFamily = "Lato";
+  Chart.defaults.global.defaultFontFamily = 18;
+  Chart.defaults.global.defaultFontFamily = "#777";
+
+  let popchart = new Chart(varchart, {
+    type: "pie", //bar horizontalBar pie line doughnut radar polarArea
+    data: {
+      labels: [
+        result.categoryArr[0],
+        result.categoryArr[1],
+        result.categoryArr[2],
+        result.categoryArr[3]
+      ],
+      datasets: [
+        {
+          label: "Population",
+          data: [
+            result.expenseArr[0],
+            result.expenseArr[1],
+            result.expenseArr[2],
+            result.expenseArr[3]
+          ],
+          backgroundColor: ["green", "red", "yellow", "purple", "brown"],
+          borderWidth: 1,
+          borderColor: "darkblue",
+          hoverBorderWidth: 3,
+          hoverBorderColor: "black"
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Expenses",
+        fontsize: 25
+      },
+
+      legend: {
+        display: true,
+        position: "right",
+        labels: {
+          fontColor: "black"
+        }
       },
       options: {
-        title: {
-          display: true,
-          text: "Expenses",
-          fontsize: 25
-        },
-        legend: {
-          display: true,
-          position: "right",
-          labels: {
-            fontColor: "black"
+        animation: {
+          duration: 5000,
+          onProgress: function(animation) {
+            progress.value = animation.currentStep / animation.numSteps;
           }
-        },
-        tooltips: {
-          enabled: true
         }
+      },
+      tooltips: {
+        enabled: true
       }
-    });
+    }
   });
+}
+
+function createLineChart(result) {
+  let varchart1 = document.getElementById("mychart1").getContext("2d");
+
+  Chart.defaults.global.defaultFontFamily = "Lato";
+  Chart.defaults.global.defaultFontFamily = 18;
+  Chart.defaults.global.defaultFontFamily = "#777";
+
+  let popchart1 = new Chart(varchart1, {
+    type: "bar", //bar horizontalBar pie line doughnut radar polarArea
+    data: {
+      labels: [
+        result.categoryArr[0],
+        result.categoryArr[1],
+        result.categoryArr[2],
+        result.categoryArr[3]
+      ],
+      datasets: [
+        {
+          label: "Population",
+          data: [
+            result.expenseArr[0],
+            result.expenseArr[1],
+            result.expenseArr[2],
+            result.expenseArr[3]
+          ],
+          backgroundColor: ["green", "red", "yellow", "purple", "brown"],
+          borderWidth: 1,
+          borderColor: "#777",
+          hoverBorderWidth: 3,
+          hoverBorderColor: "black"
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Expenses",
+        fontsize: 25
+      },
+      legend: {
+        display: true,
+        position: "right",
+        labels: {
+          fontColor: "black"
+        }
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
+  });
+}
+
+function createdoghnutChart(result) {
+  let varchart = document.getElementById("mychart2").getContext("2d");
+
+  Chart.defaults.global.defaultFontFamily = "Lato";
+  Chart.defaults.global.defaultFontFamily = 18;
+  Chart.defaults.global.defaultFontFamily = "#777";
+
+  let popchart = new Chart(varchart, {
+    type: "doughnut", //bar horizontalBar pie line doughnut radar polarArea
+    data: {
+      labels: [
+        result.categoryArr[0],
+        result.categoryArr[1],
+        result.categoryArr[2],
+        result.categoryArr[3]
+      ],
+      datasets: [
+        {
+          label: "Population",
+          data: [
+            result.expenseArr[0],
+            result.expenseArr[1],
+            result.expenseArr[2],
+            result.expenseArr[3]
+          ],
+          backgroundColor: ["green", "red", "yellow", "purple", "brown"],
+          borderWidth: 1,
+          borderColor: "darkblue",
+          hoverBorderWidth: 3,
+          hoverBorderColor: "black"
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Expenses",
+        fontsize: 25
+      },
+
+      legend: {
+        display: true,
+        position: "right",
+        labels: {
+          fontColor: "black"
+        }
+      },
+      options: {
+        animation: {
+          duration: 5000,
+          onProgress: function(animation) {
+            progress.value = animation.currentStep / animation.numSteps;
+          }
+        }
+      },
+      tooltips: {
+        enabled: true
+      }
+    }
+  });
+}
