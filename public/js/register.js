38bc9aa93bff8cdb3a51aff3cd73dd4495fffb5f
AFWebDev <andrewfariaj@gmail.com>
2019-10-03 22:14:03 -0400

    register page, user exist toast message!

--- public/js/register.js
+++ public/js/register.js
@@ -5,18 +5,30 @@ $(document).ready(function() {
 
   $(".registerForm").submit(e => {
     e.preventDefault();
-  });
+    console.log(e);
+    console.log($(this));
 
-  $.ajax({
-    url: "/users/login/", //give your url here
-    type: "POST",
-    dataType: "json",
-    data: logindata,
-    success: function(data) {
-      //  alert(data);    do your stuff
-    },
-    error: function(data) {
-      //  alert(data);    do your stuff
-    }
+    let user = $("#username").val();
+    let pass = $("#password").val();
+    let loginData = {
+      username: user,
+      password: pass
+    };
+    $.ajax({
+      url: "/api/auth/register", //give your url here
+      type: "POST",
+      dataType: "json",
+      data: loginData,
+      success: function(data) {
+        console.log(data);
+        //  alert(data);    do your stuff
+      },
+      error: function(data) {
+        console.log(data.responseJSON);
+        let err = data.responseJSON.err;
+        M.toast({ html: err });
+        //  alert(data);    do your stuff
+      }
+    });
   });
 });
