bd11c2851fdaa18741521418b1dcd41c267d75cd
Manjyot Thandi <manjyot.thandi@gmail.com>
2019-10-03 20:59:39 -0400

    Added in budget routes, and logic, No Front end

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -77,6 +77,32 @@ module.exports = function(app) {
       });
   });
 
+  // TAKES INPUT FROM BUDGET PAGE AND UPDATES BUDGET TABLE. ONLY RECORDS ACTIVE BUDGET IN TABLE
+  app.post("/api/add/budget", (req, resp) => {
+    let id = req.session.passport.user.id;
+    let fromDate = req.body.fromDate;
+    let toDate = req.body.toDate;
+    let budget = req.body.Amount;
+    db.Budget.destroy({
+      where: {
+          UserId: id
+      }
+  }).then(function(){
+    db.Budget.create({
+      date_from: fromDate,
+      date_to: toDate,
+      budget: budget,
+      UserId: id,
+    }).then(function() {
+      console.log("ALL DONE INSERTS");
+      //resp.sendStatus(200);
+      resp.redirect("/budget");
+    });
+  })
+   // console.log(req.body);
+  });
+
+
   //GET LOCATIONS FROM USER IN DB API
   //SENDS BACK DATA BASED ON EXPENSE PURCHASE LOCATIONS IN JSON TO CLIENT.
   app.get("/api/get/locations", (req, resp) => {
@@ -91,3 +117,6 @@ module.exports = function(app) {
     });
   });
 };
+
+
+
