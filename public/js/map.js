134a814e5db6ffa483f3de967efb106d109f429a
AFWebDev <andrewfariaj@gmail.com>
2019-10-05 21:21:21 -0400

    map date picker complete

--- public/js/map.js
+++ public/js/map.js
@@ -6,9 +6,6 @@ $(document).ready(function() {
   $("select").formSelect();
   //==============================
 
-  //Define Global Vars
-  let markers = [];
-
   //BUILD THE MAP
   let myMap = L.map("weatherMap").setView([43.6507, -79.347015], 9);
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -19,9 +16,56 @@ $(document).ready(function() {
     accessToken: "pk.eyJ1IjoiYWZ3ZWJkZXYiLCJhIjoiY2sxMDB2MTJyMDB6NDNocDJ5ZTRzem5yNCJ9.-GMQ7KKaj_kPsf4ONmj6uQ"
   }).addTo(myMap);
 
-  const getNewData = () => {
-    markers = [];
-    fetch("/api/get/locations")
+  // const getNewData = () => {
+  //   markers = [];
+  //   fetch("/api/get/locations")
+  //     .then(res => res.json())
+  //     .then(result => {
+  //       result.forEach(purchase => {
+  //         console.log(purchase);
+  //         let total = purchase.total;
+  //         let { address, longitude, latitude } = purchase.Location;
+  //         console.log(address, longitude, latitude);
+  //         markers.push([latitude, longitude, { address: address, total: total }]);
+  //         let table = $("tbody");
+  //         table.append(`<tr><td>${address}</td><td>$${total}</td></tr>`);
+  //       });
+
+  //       for (var i = 0; i < markers.length; i++) {
+  //         let lat = markers[i][0];
+  //         let lon = markers[i][1];
+  //         let { address, total } = markers[i][2];
+  //         console.log(address, total);
+
+  //         var markerLocation = new L.LatLng(lat, lon);
+  //         var marker = new L.Marker(markerLocation);
+
+  //         myMap.addLayer(marker);
+  //         marker.bindPopup(`<div><b><span>${address}</span></b><br><span>Total Spent Here: $${total}</span></div>`);
+  //       }
+  //     })
+  //     .catch(err => {
+  //       throw err;
+  //     });
+  // }; //GET DATA FUNC
+  // getNewData();
+
+  //Define Global Vars
+  let markerArray = [];
+
+  //NEW
+  let locGroup;
+
+  const getData = date => {
+    if (locGroup) {
+      locGroup.clearLayers();
+    }
+    if (!date) {
+      date = "day";
+    }
+    let table = $("tbody");
+    table.html("");
+    fetch(`/api/get/locations/${date}`)
       .then(res => res.json())
       .then(result => {
         result.forEach(purchase => {
@@ -29,43 +73,32 @@ $(document).ready(function() {
           let total = purchase.total;
           let { address, longitude, latitude } = purchase.Location;
           console.log(address, longitude, latitude);
-          markers.push([latitude, longitude, { address: address, total: total }]);
-          let table = $("tbody");
+
+          //ADD TABLE DATA
+
           table.append(`<tr><td>${address}</td><td>$${total}</td></tr>`);
+
+          var marker = new L.Marker([latitude, longitude]);
+          marker.bindPopup(`
+        <div>
+          <b><span>${address}</span></b>
+          <br><span>Total Spent Here: $${total}</span>
+        </div>`);
+          markerArray.push(marker);
         });
-
-        for (var i = 0; i < markers.length; i++) {
-          let lat = markers[i][0];
-          let lon = markers[i][1];
-          let { address, total } = markers[i][2];
-          console.log(address, total);
-
-          var markerLocation = new L.LatLng(lat, lon);
-          var marker = new L.Marker(markerLocation);
-
-          myMap.addLayer(marker);
-          marker.bindPopup(`<div><b><span>${address}</span></b><br><span>Total Spent Here: $${total}</span></div>`);
-        }
+      })
+      .then(() => {
+        locGroup = new L.layerGroup(markerArray).addTo(myMap);
       })
       .catch(err => {
         throw err;
       });
-  }; //GET DATA FUNC
-  getNewData();
+  };
 
-  //NEW
+  $(document).on("change", "#selectday", e => {
+    let date = e.target.value;
+    getData(date);
+  });
 
-  fetch("/api/get/locations")
-    .then(res => res.json())
-    .then(result => {
-      result.forEach(purchase => {
-        console.log(purchase);
-        let total = purchase.total;
-        let { address, longitude, latitude } = purchase.Location;
-        console.log(address, longitude, latitude);
-      });
-    })
-    .catch(err => {
-      throw err;
-    });
-});
+  getData();
+}); //END
--- public/js/profile.js
+++ public/js/profile.js
@@ -28,9 +28,7 @@ $(document).ready(function() {
   function createChart(result) {
     let randomColorArray = [];
     for (let i = 0; i < result.expenseArr.length; i++) {
-      randomColorArray.push(
-        colorArray[Math.floor(Math.random() * colorArray.length)]
-      );
+      randomColorArray.push(colorArray[Math.floor(Math.random() * colorArray.length)]);
     }
 
     //Start of Pi Chart
@@ -86,137 +84,130 @@ $(document).ready(function() {
       }
     });
 
-<<<<<<< HEAD
-  //CREATE BAR GRAPH
-  function createLineChart(result) {
-    let expenseData = result.expenseArr.map(expense => {
-      return parseInt(expense);
-    });
-    console.log(expenseData);
-    let varChart = document.getElementById("mychart1").getContext("2d");
-    console.log(result);
-=======
-    //End of pi chart
-    //Start of line Chart
+    //CREATE BAR GRAPH
+    function createLineChart(result) {
+      let expenseData = result.expenseArr.map(expense => {
+        return parseInt(expense);
+      });
+      console.log(expenseData);
+      let varChart = document.getElementById("mychart1").getContext("2d");
+      console.log(result);
+      Chart.defaults.global.defaultFontFamily = "Lato";
+      Chart.defaults.global.defaultFontFamily = 18;
+      Chart.defaults.global.defaultFontFamily = "#777";
 
-    let varChart1 = document.getElementById("mychart1").getContext("2d");
-    // console.log(result);
->>>>>>> ac654db5b5cc3aec322d12aebed7ed39ca3d6058
-    Chart.defaults.global.defaultFontFamily = "Lato";
-    Chart.defaults.global.defaultFontFamily = 18;
-    Chart.defaults.global.defaultFontFamily = "#777";
-
-    let popchart1 = new Chart(varChart1, {
-      type: "bar", //bar horizontalBar pie line doughnut radar polarArea
-      data: {
-        labels: result.categoryArr,
-        datasets: [
-          {
-            data: [...expenseData],
-            backgroundColor: randomColorArray,
-            borderWidth: 1,
-            borderColor: "#777",
-            hoverBorderWidth: 3,
-            hoverBorderColor: "black"
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: "Expenses",
-          fontsize: 25
-        },
-        legend: {
-          display: false,
-          position: "right",
-          labels: {
-            fontColor: "black"
-          }
-        },
-        tooltips: {
-          enabled: true
-        }
-      }
-    });
-
-    //End of line chart
-    // Start of doughnut Chart
-
-    let varChart2 = document.getElementById("mychart2").getContext("2d");
-
-    Chart.defaults.global.defaultFontFamily = "Lato";
-    Chart.defaults.global.defaultFontFamily = 18;
-    Chart.defaults.global.defaultFontFamily = "#777";
-
-    let popchart2 = new Chart(varChart2, {
-      type: "doughnut", //bar horizontalBar pie line doughnut radar polarArea
-      data: {
-        labels: result.categoryArr,
-        datasets: [
-          {
-            label: "Population",
-            data: expenseData,
-            backgroundColor: randomColorArray,
-            borderWidth: 1,
-            borderColor: "darkblue",
-            hoverBorderWidth: 3,
-            hoverBorderColor: "black"
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: "Expenses",
-          fontsize: 25
-        },
-
-        legend: {
-          display: true,
-          position: "right",
-          labels: {
-            fontColor: "black"
-          }
+      let popchart1 = new Chart(varChart1, {
+        type: "bar", //bar horizontalBar pie line doughnut radar polarArea
+        data: {
+          labels: result.categoryArr,
+          datasets: [
+            {
+              data: [...expenseData],
+              backgroundColor: randomColorArray,
+              borderWidth: 1,
+              borderColor: "#777",
+              hoverBorderWidth: 3,
+              hoverBorderColor: "black"
+            }
+          ]
         },
         options: {
-          animation: {
-            duration: 5000,
-            onProgress: function(animation) {
-              progress.value = animation.currentStep / animation.numSteps;
+          title: {
+            display: true,
+            text: "Expenses",
+            fontsize: 25
+          },
+          legend: {
+            display: false,
+            position: "right",
+            labels: {
+              fontColor: "black"
             }
+          },
+          tooltips: {
+            enabled: true
           }
-        },
-        tooltips: {
-          enabled: true
         }
-      }
-    });
+      });
 
-    $(document).on("change", "select", function(e) {
-      popchart.destroy();
-      popchart1.destroy();
-      popchart2.destroy();
+      //End of line chart
+      // Start of doughnut Chart
+
+      let varChart2 = document.getElementById("mychart2").getContext("2d");
+
+      Chart.defaults.global.defaultFontFamily = "Lato";
+      Chart.defaults.global.defaultFontFamily = 18;
+      Chart.defaults.global.defaultFontFamily = "#777";
+
+      let popchart2 = new Chart(varChart2, {
+        type: "doughnut", //bar horizontalBar pie line doughnut radar polarArea
+        data: {
+          labels: result.categoryArr,
+          datasets: [
+            {
+              label: "Population",
+              data: expenseData,
+              backgroundColor: randomColorArray,
+              borderWidth: 1,
+              borderColor: "darkblue",
+              hoverBorderWidth: 3,
+              hoverBorderColor: "black"
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: "Expenses",
+            fontsize: 25
+          },
+
+          legend: {
+            display: true,
+            position: "right",
+            labels: {
+              fontColor: "black"
+            }
+          },
+          options: {
+            animation: {
+              duration: 5000,
+              onProgress: function(animation) {
+                progress.value = animation.currentStep / animation.numSteps;
+              }
+            }
+          },
+          tooltips: {
+            enabled: true
+          }
+        }
+      });
+
+      $(document).on("change", "select", function(e) {
+        popchart.destroy();
+        popchart1.destroy();
+        popchart2.destroy();
+      });
+    }
+
+    //oncChange handler for date dropdown
+    $(document).on("change", "select", function() {
+      let newdata = {
+        newdata: this.value
+      };
+
+      fetch("/api/profile", {
+        method: "post",
+        body: JSON.stringify(newdata), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(result => result.json())
+        .then(res => {
+          console.log(JSON.stringify(res));
+          createChart(res);
+        });
     });
   }
-
-  //oncChange handler for date dropdown
-  $(document).on("change", "select", function() {
-    let newdata = {
-      newdata: this.value
-    };
-
-    fetch("/api/profile", {
-      method: "post",
-      body: JSON.stringify(newdata), // data can be `string` or {object}!
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(result => result.json())
-      .then(res => {
-        console.log(JSON.stringify(res));
-        createChart(res);
-      });
-  });
 }); //^^^EVERYTHING SHOULD BE IN HERE
