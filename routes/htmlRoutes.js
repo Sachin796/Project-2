11239794e8dbd3adbffb7bb7a3cda8492e1af3d9
Manjyot Thandi <manjyot.thandi@gmail.com>
2019-10-07 18:50:15 -0400

    LOL

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -52,12 +52,13 @@ module.exports = function(app) {
       where: {
         UserId: id
       },
+      limit: 5, 
       order: [['createdAt', 'DESC']]
     })
       .then(res => {
         console.log("THIS IS THE QUERY RESULT ----------------------------------------" + res[0].dataValues);
         // loop through results and push to correct objects.
-        for (i = 0; i < 5; i++) {
+        for (i = 0; i < res.length; i++) {
           let amtSpent = res[i].dataValues.amount_spent;
           let category = res[i].dataValues.category;
           let itemName = res[i].dataValues.item_name;
@@ -67,6 +68,7 @@ module.exports = function(app) {
         resp.render("expensePage", { layout: "expense", expense, loggedIn: true });
       })
       .catch(err => {
+        console.log(err)
         console.log("HELLO");
         resp.render("expensePage", { layout: "expense", loggedIn: true });
       });
