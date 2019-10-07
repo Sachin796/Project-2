6074b58e9891a3b6be6805f315f20cf9975b9da4
AFWebDev <andrewfariaj@gmail.com>
2019-10-07 19:23:04 -0400

    fix

--- public/js/expense.js
+++ public/js/expense.js
@@ -3,39 +3,46 @@ $(document).ready(function() {
   $(".sidenav").sidenav();
   $(".sidenav").css({ zIndex: 9999 });
   $(".dropdown-trigger").dropdown();
-
+  $("select").formSelect();
   $(".expenseForm").submit(e => {
     e.preventDefault();
-    console.log("here1");
+    // Validation
     let address = $("#address").val();
     let country = $("#country").val();
     let category = $("#category").val();
     let itemName = $("#itemName").val();
     let amount = $("#amount").val();
-    let expenseData = {
-      Address: address,
-      Country: country,
-      Amount: amount,
-      Category: category,
-      itemName: itemName
-    };
-    console.log(expenseData);
-    console.log("here2");
-    //Request
-    $.ajax({
-      url: "/api/add/expense", //give your url here
-      type: "POST",
-      dataType: "json",
-      data: expenseData,
-      error: function(data) {
-        console.log("here3");
-        if (data.status === 401) {
-          let err = data.responseJSON.error;
-          M.toast({ html: err });
-        } else {
-          document.location.href = "/expense";
+    if (amount < 0) {
+      M.toast({ html: "Please enter a valid expense amount" });
+      return false;
+    } else if (amount === "" || address === "" || country === "" || itemName === "" || category === null) {
+      M.toast({ html: "Please ensure all fields are populated" });
+      return false;
+    } else {
+      let expenseData = {
+        Address: address,
+        Country: country,
+        Amount: amount,
+        Category: category,
+        itemName: itemName
+      };
+      console.log(expenseData);
+      //Request
+      $.ajax({
+        url: "/api/add/expense", //give your url here
+        type: "POST",
+        dataType: "json",
+        data: expenseData,
+        error: function(data) {
+          console.log("here3");
+          if (data.status === 401) {
+            let err = data.responseJSON.error;
+            M.toast({ html: err });
+          } else {
+            document.location.href = "/expense";
+          }
         }
-      }
-    });
+      });
+    }
   });
 });
